ba147255d965514a478311677c0f124028de2d53
Joanna Liana <joanna_liana@outlook.com>
2021-06-13 10:11:42 +0200

    Add EuropeanVatProvider specs

--- src/EuropeanVatProvider.test.ts
+++ src/EuropeanVatProvider.test.ts
@@ -1,15 +1,103 @@
+import { CountryNotSupportedException } from './CountryNotSupportedException';
 import { EuropeanVatProvider } from './EuropeanVatProvider';
 import { Type } from './Product/Type';
 
 describe('EuropeanVatProvider', () => {
-  it('Returns 5 percentage for books in Poland', async () => {
-    // given
-    const vatProvider = new EuropeanVatProvider();
+  let vatProvider: EuropeanVatProvider;
 
-    // when
-    const polishBooksVat = vatProvider.getVatFor('Poland', Type.BOOK);
+  beforeEach(() => {
+    vatProvider = new EuropeanVatProvider();
+  });
 
-    // then
-    expect(polishBooksVat).toEqual(0.05);
+  describe('Poland', () => {
+    const country = 'Poland';
+
+    it.each([
+      Type.BABY,
+      Type.BOOK
+    ])('Returns 5 percentage for %s products', (type) => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, type);
+
+      // then
+      expect(vat).toEqual(0.05);
+    });
+
+    it('Returns 8 percentage for FOOD products', () => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, Type.FOOD);
+
+      // then
+      expect(vat).toEqual(0.08);
+    });
+
+    it.each(
+      Object.values(Type).filter(t => ![Type.BABY, Type.BOOK, Type.FOOD].includes(t))
+    )('Returns 23 percentage for products other than BABY, FOOD and BOOK - %s', (type) => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, type);
+
+      // then
+      expect(vat).toEqual(0.23);
+    });
+  });
+
+  describe('Germany', () => {
+    const country = 'Germany';
+
+    it.each([
+      Type.BABY,
+      Type.BOOK,
+      Type.FOOD
+    ])('Returns 4 percentage for %s products', (type) => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, type);
+
+      // then
+      expect(vat).toEqual(0.04);
+    });
+
+    it('Returns 10 percentage for CLOTHES products', () => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, Type.CLOTHES);
+
+      // then
+      expect(vat).toEqual(0.1);
+    });
+
+    it.each(
+      Object.values(Type).filter(t => ![Type.BABY, Type.BOOK, Type.FOOD, Type.CLOTHES].includes(t))
+    )('Returns 21 percentage for products other than CLOTHES, BABY, FOOD and BOOK - %s', (type) => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, type);
+
+      // then
+      expect(vat).toEqual(0.21);
+    });
+  });
+
+  describe('Denmark', () => {
+    const country = 'Denmark';
+
+    it.each(Object.values(Type))('Returns 8 percentage for all product types - %s', (type) => {
+      // given, when
+      const vat = vatProvider.getVatFor(country, type);
+
+      // then
+      expect(vat).toEqual(0.08);
+    });
+  });
+
+  describe('Unsupported country', () => {
+    it('Throws when given an unsupported country', () => {
+      expect(() => vatProvider.getVatFor('unsupported', Type.BABY)).toThrowError(CountryNotSupportedException);
+    });
+
+    it.each([
+      'denmark',
+      'POLAND'
+    ])('Throws when a supported country name is not in Pascal case - %s', (country) => {
+      expect(() => vatProvider.getVatFor(country, Type.BABY)).toThrowError(CountryNotSupportedException);
+    });
   });
 });
